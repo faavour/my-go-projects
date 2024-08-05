bf00b31752bd0ab89d4dff89ec2aa230a2c136d7
faavour <favourjohn862@gmail.com>
2024-08-05 15:00:30 +0100

    added first case

--- cmd-to-do-list/main.go
+++ cmd-to-do-list/main.go
@@ -7,23 +7,34 @@ import (
 	"bufio"
 )
 
-func main()  {
+func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	// texts := []string{}
+	tasks := []string{}
 
-	for scanner.Scan() {
+	for {
 		fmt.Println("Enter command (add, remove, complete, lists, or exit):")
 		//Read the input from the user
 
-		input := scanner.Text()
+		input := scanner.Scan()
 
 		//process the input (print the input)
 		fmt.Println("You entered:", input)
 
 		//optionally break the loop based on some condition
 
-		if input == "exit"{
+		if !input {
 			break
 		}
+
+		output := scanner.Text()
+		switch output {
+		case "add":
+			
+			fmt.Println("Task added Successfully")
+			task := scanner.Text()
+			tasks = append(tasks, task)
+			fmt.Println(tasks)
+		}
 	}
+	
 }
